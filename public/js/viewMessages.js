9c54be5b18bfdc85b9bf16a8b3d6f5fdc6d9f3e5
Ethan Byeon <ethanbyeon@gmail.com>
2021-07-22 18:41:00 +0000

    fixed viewMessages to be consistent with writeMessages

--- public/js/viewMessages.js
+++ public/js/viewMessages.js
@@ -1,20 +1,28 @@
 const getMessages = () => {
-    const passcode = document.querySelector("#passcode");
+    const password = document.querySelector("#passcode");
+    // console.log("PASSWORD:", password.value)
     
     const messagesRef = firebase.database().ref();
     messagesRef.on('value', (snapshot) => {
         const data = snapshot.val();
-        console.log(data);
+        // console.log("DATA:", data);
+
+        const message = document.querySelector("#message");
+        if (password.value.length == "") {
+            message.innerHTML = "Please enter a password."
+            return
+        }
+        if (password.value.length > 10) {
+            message.innerHTML = "Password exceeds 10 characters!"
+            return
+        }
 
         for (let key in data) {
             console.log(key, data[key]);
-            if (key === passcode.value) {
+            if (data[key].password === password.value) {
                 console.log("Match found")
-                const message = document.querySelector("#message");
-                message.innerHTML = data[key]
+                message.innerHTML = data[key].message
             }
         }
     })
-
-    console.log("Yo.", passcode.value);
 }
