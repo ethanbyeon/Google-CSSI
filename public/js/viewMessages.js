478f2e2063b0df6583cf9f733480e6c332bf2011
Ethan Byeon <ethanbyeon@gmail.com>
2021-07-22 17:05:36 +0000

    found a match for the user's passcode.

--- public/js/viewMessages.js
+++ public/js/viewMessages.js
@@ -8,6 +8,11 @@ const getMessages = () => {
 
         for (let key in data) {
             console.log(key, data[key]);
+            if (key === passcode.value) {
+                console.log("Match found")
+                const message = document.querySelector("#message");
+                message.innerHTML = data[key]
+            }
         }
     })
 
