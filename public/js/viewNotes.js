7a47347f870d3821d0b3103249f63031f28fa51f
Ethan Byeon <ethanbyeon@gmail.com>
2021-07-28 18:57:26 +0000

    added edit and delete functionality

--- public/js/viewNotes.js
+++ public/js/viewNotes.js
@@ -16,6 +16,7 @@ window.onload = (event) => {
 
 const getNotes = (userId) => {
   const notesRef = firebase.database().ref(`users/${userId}`);
+//   var notesTitle = firebase.database().ref(`users/${userId}/${noteId}`).orderByChild('title');
   notesRef.on('value', (snapshot) => {
     const data = snapshot.val();
     renderDataAsHtml(data);
@@ -24,16 +25,17 @@ const getNotes = (userId) => {
 
 const renderDataAsHtml = (data) => {
   let cards = ``;
+
   for (const noteItem in data) {
     const note = data[noteItem];
     // For each note create an HTML card
-    cards += createCard(note)
+    cards += createCard(note, noteItem)
   };
   // Inject our string of HTML into our viewNotes.html page
   document.querySelector('#app').innerHTML = cards;
 };
 
-const createCard = (note) => {
+const createCard = (note, noteId) => {
   return `
     <div class="column is-one-quarter">
       <div class="card">
@@ -43,7 +45,58 @@ const createCard = (note) => {
         <div class="card-content">
           <div class="content">${note.text}</div>
         </div>
+        <footer class="card-footer">
+            <a href="#" class="card-footer-item" onclick="editNote('${noteId}')">
+                Edit
+            </a>
+            <a href="#" class="card-footer-item" onclick="deleteNote('${noteId}')">
+                Delete
+            </a>
+        </footer>
       </div>
     </div>
-  `;
+    `;
+}
+
+const deleteNote = (noteId) => {
+    const deleteNoteModal = document.querySelector('#deleteNoteModal')
+    deleteNoteModal.classList.toggle('is-active')
+
+    firebase.database().ref(`users/${googleUserId}/${noteId}`).remove()
+}
+
+const editNote = (noteId) => {
+    const editNoteModal = document.querySelector('#editNoteModal')
+
+    const notesRef = firebase.database().ref(`users/${googleUserId}`)
+    notesRef.on('value', (snapshot) => {
+        const data = snapshot.val()
+        const note = data[noteId]
+        document.querySelector('#editTitleInput').value = note.title
+        document.querySelector('#editTextInput').value = note.text
+        document.querySelector('#editNoteId').value = noteId
+    })  
+    editNoteModal.classList.toggle('is-active')
+}
+
+const closeDeleteModal = () => {
+    const deleteNoteModal = document.querySelector('#deleteNoteModal')
+    deleteNoteModal.classList.toggle('is-active')
+}
+
+const closeEditModal = () => {
+    const editNoteModal = document.querySelector('#editNoteModal')
+    editNoteModal.classList.toggle('is-active')
+}
+
+const saveEditedNote = () => {
+    const noteTitle = document.querySelector('#editTitleInput').value
+    const noteText = document.querySelector('#editTextInput').value
+    const noteId = document.querySelector('#editNoteId').value
+    const noteEdits = {
+        title: noteTitle,
+        text: noteText,
+    }
+    firebase.database().ref(`users/${googleUserId}/${noteId}`).update(noteEdits)
+    closeEditModal()
 }
